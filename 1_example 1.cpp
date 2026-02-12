e335dbd9d515e2ec22dababcdf9f73ebd466a549
Hidayat-ullah <hidayatkhan4877@gmail.com>
2026-02-12 10:44:29 -0800

    Add files via upload

---
+++ 1_example 1.cpp
@@ -0,0 +1,17 @@
+     // Fig. 1.2: fig01_02.cpp //means the compiler ignore this line.
+     // assd
+    // A first program in C++.//it tell that is the first c++program.
+#include <iostream> //The line include the input-output libary.ye input lene ki liye awr cout print 
+karnay ki leye use hota hai .with out this printing is not possible.
+using namespace std;//agar hum ye function likain to per humain serf cout likna parta hai.
+awr agar pehly nahi laka to per cout ka sath std; use hota hai. 
+int main()// ye sab say important function hai,jahan per program shoro hota hai.
+    {
+    	/* this 
+		is 
+		you
+		multiline 
+		comment*/
+    return 0;   // ye wo function hai, jahan per program khatam hota hai.
+    } // end function main
+
