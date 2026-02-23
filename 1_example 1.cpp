9c127b60a1e7c1b1303ea0bd92a0db129060638d
Hidayat-ullah <hidayatkhan4877@gmail.com>
2026-02-23 11:37:32 -0800

    Add files via upload

---
+++ 1_example 1.cpp
@@ -0,0 +1,25 @@
+     // Fig. 1.2: fig01_02.cpp//
+     // assd
+    // A first program in C++.// ye batata hai ka ye c++ ka pehla program hai.
+#include <iostream>//yeh iostream library ko include karta hai. es se hum cout or cin use karta 
+sakta hai.
+using namespace std;//es se humain haar dafa std;cout likhne ki zararut nahi hoti.sirf cout likna 
+parta hai.
+int main()//ye sab sy important function hai.jahan sy program shoro hota hai.int ka matlb yeh 
+function integer value return karega.
+    {//ye open bracket hai.is ka ander main fuction ka code likha jata hai.
+    	/* this 
+		is 
+		you
+		multiline 
+		comment*/
+    return 0;   // ye wo function hai jahan per program successfully khatam hota hai.
+    0 ka matlb hai no error.
+    } // ye closed bracket hai.yeha per main function khtam hota hai.
+    
+    //Errors
+    // iostream main jo error aya hai es ko removal karo
+    //using namespace main error aya hai es ko removal karo 
+    //int mai error hai es mai int ka bad main nahi likha hai es ki sahi karo
+    //bracket mai jo ghalte wo ye hai ka { ki jaga ( wala lika hai es ko sahi karo 
+
